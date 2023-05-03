4e1cf9452015a64095a5077c572c3f7f423a4c56
Sergey <sergeychikin1981@gmail.com>
2023-05-03 21:40:34 +0300

    First commit.

---
+++ src/main/java/pro/sky/skyproshoppingcart/SkyProShoppingCartApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyproshoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProShoppingCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyProShoppingCartApplication.class, args);
+	}
+
+}
